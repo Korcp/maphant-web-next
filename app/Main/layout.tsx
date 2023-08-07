4117a19ea61cc306fd42c7da6602d4323cba0dc1
yunjinsu <wlstn4940@gmail.com>
2023-08-07 17:22:26 +0900

    Layout 수정

--- app/Main/layout.tsx
+++ app/Main/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ko">
+    <html lang="en" className={styles.html}>
       <body className={inter.className}>
         <div className={styles.layout}>
           <MainHeader />
