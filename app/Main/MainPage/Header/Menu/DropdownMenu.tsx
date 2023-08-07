974a2897f9a187a9b9afe04b0fb721a0385a2f7b
hyun <pm6567@naver.com>
2023-08-07 15:36:18 +0900

    feat: Logout 구현 완료

--- app/Main/MainPage/Header/Menu/DropdownMenu.tsx
+++ app/Main/MainPage/Header/Menu/DropdownMenu.tsx
@@ -1,13 +1,24 @@
 "use client";
-import React, { forwardRef } from "react";
+import React, { forwardRef, useReducer } from "react";
 import styles from "./DropdwonMenu.module.css";
+import { useRouter } from "next/navigation";
 
 const DropdownMenu = () => {
+  const router = useRouter();
+
+  const Logout = () => {
+    localStorage.clear();
+
+    router.push("/");
+  };
+
   return (
     <ul className={styles.menulist}>
       <li className={styles.userInfo}>회원 정보</li>
       <li className={styles.history}>활동 내역</li>
-      <li className={styles.logout}>로그아웃</li>
+      <li className={styles.logout} onClick={Logout}>
+        로그아웃
+      </li>
     </ul>
   );
 };
