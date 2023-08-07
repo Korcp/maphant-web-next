ecd95f05639b0f2dabcec10239c78d30cc7bf1f7
yunjinsu <wlstn4940@gmail.com>
2023-08-07 17:20:30 +0900

    Logout 추가

--- app/Main/MainPage/Header/Menu/DropdownMenu.tsx
+++ app/Main/MainPage/Header/Menu/DropdownMenu.tsx
@@ -2,13 +2,25 @@
 import React, { forwardRef } from "react";
 import styles from "./DropdwonMenu.module.css";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const DropdownMenu = () => {
+  const router = useRouter();
+
+  const Logout = () => {
+    localStorage.clear();
+
+    router.push("/");
+  };
   return (
     <ul className={styles.menulist}>
-      <Link href="/Main/MypagePW" className={styles.userInfo}>회원 정보</Link>
+      <Link href="/Main/MypagePW" className={styles.userInfo}>
+        회원 정보
+      </Link>
       <li className={styles.history}>활동 내역</li>
-      <li className={styles.logout}>로그아웃</li>
+      <li className={styles.logout} onClick={Logout}>
+        로그아웃
+      </li>
     </ul>
   );
 };
