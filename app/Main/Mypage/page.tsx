0ba48e053ddfb570a690ba60ef46b6a147bdc638
hyun <pm6567@naver.com>
2023-08-08 20:06:39 +0900

    feat: 회원수정 페이지 정보 받아서 input에 반영 및 UI 조금 수정

--- app/Main/Mypage/page.tsx
+++ app/Main/Mypage/page.tsx
@@ -5,65 +5,120 @@ import Link from "next/link";
 import styles from "./Mypage.module.css";
 
 function page() {
-  const ex = localStorage.getItem("token");
-  console.log(ex);
+  const [userData, setUserData] = useState({
+    id: null,
+    univId: null,
+    email: "",
+    password: "",
+    nickname: "",
+    name: "",
+    sno: "",
+    phNum: "",
+    state: 0,
+    role: "",
+    agreedAt: null,
+    createdAt: "",
+    lastmodifiedAt: "",
+  });
 
+  useEffect(() => {
+    var myHeaders = new Headers();
+    myHeaders.append("x-auth", "maphant@pubKey");
+    myHeaders.append("x-timestamp", "100");
+    myHeaders.append("x-sign", "maphant@privKey");
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({});
+
+    var requestOptions: Object = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://dev.api.tovelop.esm.kr/user/changeinfo/olddata",
+      requestOptions
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setUserData(res.data), console.log(res.data);
+      })
+      .catch((error) => console.log("error", error));
+  }, []);
+  console.log(userData);
   return (
     <div className={styles.boardLayout}>
       <div className={styles.infor}>회원정보</div>
 
       <div className={styles.nickname}>닉네임 변경</div>
 
-      <input
-        type="text"
-        className={styles.nicknametype}
-        placeholder="닉네임"
-      ></input>
-
-      <div className={styles.name}>이름</div>
-      <input
-        type="text"
-        className={styles.nametype}
-        placeholder="아이디"
-      ></input>
-
-      <div className={styles.password}>현재 비밀번호</div>
-
-      <input
-        type="text"
-        className={styles.passwordtype}
-        placeholder="현재 비밀번호"
-      ></input>
-
-      <div className={styles.password1}>
-        <div className={styles.passwordword}>새로운 비밀번호 </div>
-        <div className={styles.passwordword11}>(영문 숫자 특수문자 8~20자)</div>
+      <div className={styles.emailcheck}>
+        <input
+          type="text"
+          className={styles.emailtype}
+          placeholder="닉네임"
+          value={userData.nickname}
+        ></input>
+        <button type="submit" className={styles.emailBtn}>
+          수정하기
+        </button>
       </div>
 
-      <input
-        type="text"
-        className={styles.passwordcheck}
-        placeholder="새로운 비밀번호"
-      ></input>
+      <div className={styles.name}>이름</div>
+      <div className={styles.emailcheck}>
+        <input
+          type="text"
+          className={styles.emailtype}
+          placeholder="이름"
+          value={userData.name}
+        ></input>
+        <button type="submit" className={styles.emailBtn}>
+          수정하기
+        </button>
+      </div>
+      <div className={styles.password1}>
+        <div className={styles.passwordword}>
+          새로운 비밀번호(영문 숫자 특수문자 8~20자){" "}
+        </div>
+      </div>
+
+      <div className={styles.emailcheck}>
+        <input
+          type="text"
+          className={styles.emailtype}
+          placeholder="새로운 비밀번호"
+        ></input>
+      </div>
 
       <div className={styles.password1}>
         <div className={styles.passwordword}>비밀번호 확인</div>
         <div className={styles.passwordword11}></div>
       </div>
 
-      <input
-        type="password"
-        className={styles.passwordcheck}
-        placeholder="비밀번호확인"
-      ></input>
+      <div className={styles.emailcheck}>
+        <input
+          type="text"
+          className={styles.emailtype}
+          placeholder="위의 비밀번호와 동일하게 작성"
+        ></input>
+        <button type="submit" className={styles.emailBtn}>
+          수정하기
+        </button>
+      </div>
 
       <div className={styles.phoneNum}>휴대폰 번호 수정</div>
       <div className={styles.emailcheck}>
         <input
           type="text"
-          className={styles.phonecheck}
-          placeholder="휴대폰번호 입력"
+          className={styles.emailtype}
+          placeholder="휴대폰 번호"
+          value={userData.phNum}
         ></input>
+        <button type="submit" className={styles.emailBtn}>
+          수정하기
+        </button>
       </div>
 
       <div className={styles.email}>이메일 정보</div>
@@ -72,13 +127,47 @@ function page() {
           type="text"
           className={styles.emailtype}
           placeholder="이메일 입력"
+          value={userData.email}
         ></input>
-        <button className={styles.emailBtn}>인증하기</button>
+      </div>
+
+      <div className={styles.email}>학번</div>
+      <div className={styles.emailcheck}>
+        <input
+          type="text"
+          className={styles.emailtype}
+          placeholder="임시 전공"
+          value={userData.sno}
+        ></input>
+      </div>
+
+      <div className={styles.email}>학과계열</div>
+      <div className={styles.emailcheck}>
+        <input
+          type="text"
+          className={styles.emailtype}
+          placeholder="이메일 입력"
+        ></input>
+        <button className={styles.emailBtn}>추가하기</button>
+      </div>
+
+      <div className={styles.email}>전공</div>
+      <div className={styles.emailcheck}>
+        <input
+          type="text"
+          className={styles.emailtype}
+          placeholder="임시 전공"
+        ></input>
+        <button type="submit" className={styles.emailBtn}>
+          추가하기
+        </button>
       </div>
 
       <div className={styles.end}>
         <button className={styles.delaccount}>계정 탈퇴</button>
-        <button className={styles.save}>저장하기</button>
+        <button type="submit" className={styles.save}>
+          저장하기
+        </button>
       </div>
     </div>
   );
