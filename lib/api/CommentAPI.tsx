78ffdd2cbe8ffb479e460f707bba91e5442690ed
yunjinsu <wlstn4940@gmail.com>
2023-08-18 21:18:48 +0900

    댓글 불러오기,  좋아요 api

--- lib/api/CommentAPI.tsx
+++ lib/api/CommentAPI.tsx
@@ -1,4 +1,5 @@
 import { statusResponse, dataResponse, GetAPI, PostAPI } from "./fetchAPI";
+import { CommentType } from "../type/CommentType";
 
 class CommentAPI {
   static commentPost(boardId: number, body: string, is_anonymous: number) {
@@ -7,6 +8,17 @@ class CommentAPI {
       body: body,
       is_anonymous: is_anonymous,
     });
+  }
+
+  static readComment(boardId: number, page: number, recordSize: number) {
+    return GetAPI<dataResponse<CommentType>>(`/comment/list/${boardId}`, {
+      page,
+      recordSize,
+    });
+  }
+
+  static likeComment(commentId: number) {
+    return PostAPI<statusResponse>(`/comment/like`, { commentId });
   }
 }
 
