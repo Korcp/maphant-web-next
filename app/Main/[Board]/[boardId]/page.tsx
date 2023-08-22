ba8be67856c65a2e42f41f2f1b93349d697a0181
young <uu1719@naver.com>
2023-08-22 16:40:06 +0900

    글읽기 ui 수정, 북마크 완료

--- app/Main/[Board]/[boardId]/page.tsx
+++ app/Main/[Board]/[boardId]/page.tsx
@@ -7,12 +7,14 @@ import { useEffect, useState } from "react";
 import BoardAPI from "@/lib/api/BoardAPI";
 import CommentAPI from "@/lib/api/CommentAPI";
 import { readPostType } from "@/lib/type/postType";
-import { MdThumbUp } from "react-icons/md";
+import { MdThumbUp, MdOutlineComment } from "react-icons/md";
 import { FiThumbsUp } from "react-icons/fi";
+import { AiTwotoneAlert } from "react-icons/ai";
 import { IoBookmarkOutline, IoBookmarkSharp } from "react-icons/io5";
 import Comment from "./Comment";
 import CommentList from "./CommentList";
 import { CommentType } from "@/lib/type/CommentType";
+import { BoardInfo } from "@/lib/Function/boardFunction";
 
 const page = () => {
   const router = useRouter();
@@ -24,48 +26,10 @@ const page = () => {
 
   const boardLink = parts[parts.length - 2];
 
-  let boardName: string = "";
-  let boardType: number = 0;
+  let boardName: string = BoardInfo.getBoardName(boardLink);
+  let boardType: number = BoardInfo.getBoardId(boardLink);
 
-  const detailDate = (a: string) => {
-    const milliSeconds = +new Date() - +new Date(a);
-    const seconds = milliSeconds / 1000;
-    if (seconds < 60) return `방금 전`;
-    const minutes = seconds / 60;
-    if (minutes < 60) return `${Math.floor(minutes)}분 전`;
-    const hours = minutes / 60;
-    if (hours < 24) return `${Math.floor(hours)}시간 전`;
-    const days = hours / 24;
-    if (days < 7) return `${Math.floor(days)}일 전`;
-    const weeks = days / 7;
-    if (weeks < 5) return `${Math.floor(weeks)}주 전`;
-    const months = days / 30;
-    if (months < 12) return `${Math.floor(months)}개월 전`;
-    const years = days / 365;
-    return `${Math.floor(years)}년 전`;
-  };
-
-  if (boardLink === "Free") {
-    boardName = "자유게시판";
-    boardType = 1;
-  } else if (boardLink === "Knowledge") {
-    boardName = "지식게시판";
-    boardType = 3;
-  } else if (boardLink === "QnA") {
-    boardName = "QnA";
-    boardType = 2;
-  } else if (boardLink === "Promotion") {
-    boardName = "홍보게시판";
-    boardType = 5;
-  } else if (boardLink === "Career") {
-    boardName = "취업/진로";
-    boardType = 4;
-  } else if (boardLink === "Hobby") {
-    boardName = "취미";
-    boardType = 6;
-  } else {
-    return <ErrorPage statusCode={404} />;
-  }
+  if (BoardInfo.URL_Check(boardLink)) return <ErrorPage statusCode={404} />;
 
   const getComment = () => {
     if (article) {
@@ -117,9 +81,7 @@ const page = () => {
         .then(() => {
           alert("신고되었습니다");
         })
-        .catch((err) => {
-          console.log(err);
-        });
+        .catch((err) => alert(err));
     }
   };
   const DeleteEvent = () => {
@@ -137,24 +99,18 @@ const page = () => {
 
   const starEvent = () => {
     BoardAPI.starPost(boardId)
-    .then(() => {
-      alert("스트랩하였습니다")
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+      .then(() => {
+        alert("북마크 했습니다");
+        getComment();
+      })
+      .catch((err) => alert("이미 북마크된 게시물입니다"));
   };
 
   return (
     <div className={styles.layout}>
       <div>
         <div className={styles.boardss}>
-          <div className={styles.boardernm}>
-            {boardName}
-            <button className={styles.starPost} onClick={starEvent}>
-              <IoBookmarkOutline/>
-            </button>
-          </div>
+          <div className={styles.boardernm}>{boardName}</div>
         </div>
 
         <div className={styles.postman}>
@@ -163,48 +119,60 @@ const page = () => {
           </div>
 
           <div className={styles.timeset} style={{ fontSize: ".7rem" }}>
-            {article && detailDate(article.board.createdAt)}
-            {article && article.board.isMyBoard && (
-              <div>
-                <button className={styles.fix} onClick={CollectonEvent}>
-                  수정
-                </button>
-                <button className={styles.delete} onClick={DeleteEvent}>
-                  삭제
-                </button>
+            <div className={styles.content}>
+              {article?.board.title}
+              <div className={styles.nicktime}>
+                <h4> {article?.board.userNickname}</h4>
+                <p>
+                  {article && BoardInfo.GetDetailDate(article.board.createdAt)}
+                </p>
               </div>
-            )}
+            </div>
+
+            <div>
+              <button className={styles.bTn2} onClick={likeUpEvent}>
+                {article?.board.isLike ? <MdThumbUp /> : <FiThumbsUp />}
+              </button>
+              <button className={styles.starPost} onClick={starEvent} >
+                {article?.board.isBookmarked ? (
+                  <IoBookmarkSharp />
+                ) : (
+                  <IoBookmarkOutline />
+                )}
+              </button>
+              <button className={styles.bTn1} onClick={reportEvent} disabled={article?.board.isMyBoard}>
+                <AiTwotoneAlert />
+              </button>
+              {article && article.board.isMyBoard && (
+                <div>
+                  <button className={styles.fix} onClick={CollectonEvent}>
+                    수정
+                  </button>
+                  <button className={styles.delete} onClick={DeleteEvent}>
+                    삭제
+                  </button>
+                </div>
+              )}
+            </div>
           </div>
-
-          <div className={styles.content}> {article?.board.title}</div>
-
-          {article?.board.body}
+          <div className={styles.boardbody}>{article?.board.body}</div>
 
           <div className={styles.hashtag}>
             {article?.board.tags.map((item: any, index: number) => (
               <p key={index}>#{item.name}</p>
             ))}
           </div>
-
-          <div className={styles.likeIcon}>
-            {article?.board.isLike ? <MdThumbUp /> : <FiThumbsUp />}
-            {article?.board.likeCnt}
-          </div>
-          <div className={styles.report}>
-            <button className={styles.bTn1} onClick={reportEvent}>
-              글 신고
-            </button>
-            <button className={styles.bTn2} onClick={likeUpEvent}>
-              글 추천
-            </button>
-
-            <button className={styles.bTn5}>쪽지</button>
-          </div>
         </div>
 
         {
           <div className={styles.messag}>
-            <div>댓글 {article && article?.board.commentCnt}</div>
+            <div className={styles.postman123}>
+              <div className={styles.likeIcon}>
+                {article?.board.isLike ? <MdThumbUp /> : <FiThumbsUp />}
+                {article?.board.likeCnt}
+              </div>
+              <MdOutlineComment /> {article && article?.board.commentCnt}
+            </div>
             <Comment boardId={parseInt(boardId)} getPost={getPost} />
             {article && commentList && (
               <CommentList
