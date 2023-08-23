0f0a9d73d74809e5f61af58dc605fe4edc4a2452
박현수 <pm6567@naver.com>
2023-08-24 02:22:25 +0900

    feat: 알람 받아오기

--- app/Main/MainPage/Header/Menu/NoticeMenu.tsx
+++ app/Main/MainPage/Header/Menu/NoticeMenu.tsx
@@ -6,30 +6,23 @@ import UserAPI from "@/lib/api/UserAPI";
 import UserStorage from "@/lib/storage/UserStorage";
 
 function NotifiMenu() {
-  const [alert, setalert] = useState("");
-  /*useEffect(() => {
-    UserAPI.noficationid().then((res) => {
-      console.log("알림정보", res);
-    });
-  }, []);*/
+  const [alert, setAlert] = useState([]);
+
   useEffect(() => {
     UserAPI.Getnofication().then((res) => {
-      setalert(res.data),
-        console.log("알림 리스트", res),
-        console.log("알림창창창", alert);
+      setAlert(res.data.list); // Assuming that the notifications are stored in the "list" property
     });
   }, []);
 
   return (
     <ul className={styles.noticeList}>
       <p className={styles.noticeTop}>알림</p>
-      <li className={styles.notice}>~~~글에 댓글 달림</li>
-      <li className={styles.notice}>댓글달림</li>
-      <li className={styles.notice}>댓글달림</li>
-      <li className={styles.notice}>댓글달림</li>
-      <li className={styles.notice}>댓글달림</li>
-      <li className={styles.notice}>댓글달림</li>
-      <li className={styles.notice}>댓글달림</li>
+      {alert.map((notification, index) => (
+        <li className={styles.notice} key={index}>
+          {notification.title}
+          <br /> {notification.body}
+        </li>
+      ))}
     </ul>
   );
 }
