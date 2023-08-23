eace7f54f91dacf88e7c2b503a482e689423d58f
박현수 <pm6567@naver.com>
2023-08-23 17:48:29 +0900

    alert 호출하기

--- app/Main/MainPage/Header/Menu/NoticeMenu.tsx
+++ app/Main/MainPage/Header/Menu/NoticeMenu.tsx
@@ -1,21 +1,37 @@
 "use client";
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-import styles from './NoticeMenu.module.css';
+import styles from "./NoticeMenu.module.css";
+import UserAPI from "@/lib/api/UserAPI";
+import UserStorage from "@/lib/storage/UserStorage";
 
 function NotifiMenu() {
+  const [alert, setalert] = useState("");
+  /*useEffect(() => {
+    UserAPI.noficationid().then((res) => {
+      console.log("알림정보", res);
+    });
+  }, []);*/
+  useEffect(() => {
+    UserAPI.Getnofication().then((res) => {
+      setalert(res.data),
+        console.log("알림 리스트", res),
+        console.log("알림창창창", alert);
+    });
+  }, []);
+
   return (
     <ul className={styles.noticeList}>
-        <p className={styles.noticeTop}>알림</p>
-        <li className={styles.notice}>~~~글에 댓글 달림</li>
-        <li className={styles.notice}>댓글달림</li>
-        <li className={styles.notice}>댓글달림</li>
-        <li className={styles.notice}>댓글달림</li>
-        <li className={styles.notice}>댓글달림</li>
-        <li className={styles.notice}>댓글달림</li>
-        <li className={styles.notice}>댓글달림</li>
+      <p className={styles.noticeTop}>알림</p>
+      <li className={styles.notice}>~~~글에 댓글 달림</li>
+      <li className={styles.notice}>댓글달림</li>
+      <li className={styles.notice}>댓글달림</li>
+      <li className={styles.notice}>댓글달림</li>
+      <li className={styles.notice}>댓글달림</li>
+      <li className={styles.notice}>댓글달림</li>
+      <li className={styles.notice}>댓글달림</li>
     </ul>
-  )
+  );
 }
 
-export default NotifiMenu
+export default NotifiMenu;
