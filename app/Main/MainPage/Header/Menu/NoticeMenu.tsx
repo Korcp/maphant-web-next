27993aab2d8bea03b48e0e55a3adb18ed14e45f8
박현수 <pm6567@naver.com>
2023-08-24 14:12:02 +0900

    feat: 알람 눌럿을때 읽음표시

--- app/Main/MainPage/Header/Menu/NoticeMenu.tsx
+++ app/Main/MainPage/Header/Menu/NoticeMenu.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { useState, useEffect } from "react";
 
 import styles from "./NoticeMenu.module.css";
@@ -7,7 +6,9 @@ import UserStorage from "@/lib/storage/UserStorage";
 
 function NotifiMenu() {
   const [alert, setAlert] = useState([]);
-  const [alertId, setAlertId] = useState(null);
+  const [alertId, setAlertId] = useState<number>(null);
+  const [read, setRead] = useState(false);
+  const [readItems, setReadItems] = useState([]);
 
   useEffect(() => {
     UserAPI.Getnofication().then((res) => {
@@ -15,8 +16,12 @@ function NotifiMenu() {
     });
   }, []);
 
-  const handleNotificationClick = (notificationId) => {
+  const handleNotificationClick = async (notificationId: number) => {
     setAlertId(notificationId);
+    const res = await UserAPI.noficationid(notificationId);
+    setRead(res.success);
+    setReadItems((prevItems) => [...prevItems, notificationId]);
+    console.log(res.success);
   };
 
   return (
@@ -32,6 +37,9 @@ function NotifiMenu() {
         >
           {notification.title}
           <br /> 댓글: {notification.body}
+          {readItems.includes(notification.id) && (
+            <span className={styles.readIndicator}>읽음</span>
+          )}
         </li>
       ))}
     </ul>
